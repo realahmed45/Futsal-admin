fca3d709dcbb01de5e3109fca3babaf7faff3314
realahmedali4@gmail.com <realahmedali4@gmail.com>
2025-07-09 00:35:33 +0500

    finalziing eveyrthing

--- src/components/adminApi.js
+++ src/components/adminApi.js
@@ -1,5 +1,5 @@
 // services/adminAPI.js
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://futsal-backend-wod4.onrender.com/api";
 
 export const adminAPI = {
   // Authentication
